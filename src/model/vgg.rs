fecaea71b660d5ed9923a62a95ddd532cf28baf6
Babur <baburnawyan@gmail.com>
2026-03-11 11:51:53 -0400

    Reorder Vgg methods

--- src/model/vgg.rs
+++ src/model/vgg.rs
@@ -27,21 +27,6 @@ pub struct Vgg<B: Backend> {
 }
 
 impl<B: Backend> Vgg<B> {
-    pub fn forward(&self, input: Tensor<B, 4>) -> Tensor<B, 2> {
-        let [n, c, _, _] = input.dims();
-        let reshaped_input = input.reshape([n, c, 224, 224]);
-        
-        let conv1_out = self.conv_block1.forward(reshaped_input);
-        let conv2_out = self.conv_block2.forward(conv1_out);
-        let conv3_out = self.conv_block3.forward(conv2_out);
-        let conv4_out = self.conv_block4.forward(conv3_out);
-        let conv5_out = self.conv_block5.forward(conv4_out);
-        let conv_out = conv5_out.flatten(1, 3);
-        
-        let fc_out = self.fc_block.forward(conv_out);
-        softmax(fc_out, 1)
-    }
-    
     fn construct_vgg(layer_nums: [usize; 5], batch_normalize: bool, device: &B::Device) -> Self {
         Self {
             conv_block1: ConvBlock::new(64, layer_nums[0], batch_normalize, device),
@@ -67,5 +52,20 @@ impl<B: Backend> Vgg<B> {
     
     pub fn vgg19(batch_normalize: bool, device: &B::Device) -> Self {
         Self::construct_vgg([2_usize, 2, 4, 4, 4], batch_normalize, device)
+    }
+    
+    pub fn forward(&self, input: Tensor<B, 4>) -> Tensor<B, 2> {
+        let [n, c, _, _] = input.dims();
+        let reshaped_input = input.reshape([n, c, 224, 224]);
+        
+        let conv1_out = self.conv_block1.forward(reshaped_input);
+        let conv2_out = self.conv_block2.forward(conv1_out);
+        let conv3_out = self.conv_block3.forward(conv2_out);
+        let conv4_out = self.conv_block4.forward(conv3_out);
+        let conv5_out = self.conv_block5.forward(conv4_out);
+        let conv_out = conv5_out.flatten(1, 3);
+        
+        let fc_out = self.fc_block.forward(conv_out);
+        softmax(fc_out, 1)
     }
 }
